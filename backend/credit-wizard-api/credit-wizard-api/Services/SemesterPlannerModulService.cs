5d35891ba5a524ed26f461b62a8de666d8b1e61f
Lars Bischhausen <lars.bischhausen@post.ch>
2023-05-08 15:54:58 +0200

    cleanup validation

--- backend/credit-wizard-api/credit-wizard-api/Services/SemesterPlannerModulService.cs
+++ backend/credit-wizard-api/credit-wizard-api/Services/SemesterPlannerModulService.cs
@@ -42,31 +42,10 @@ namespace credit_wizard_api.Services
 
         public async Task<int> CreateAsync(SemesterPlannerModul semesterPlannerModul, Guid userId)
         {
-            if (!await _semesterPlannerService.IsUsersPlannedSemester(userId, semesterPlannerModul.SemesterPlannerId))
-            {
-                throw new EntityNotFoundException($"User with Id {userId} has no matching planned semester with Id {semesterPlannerModul.SemesterPlannerId}");
-            }
-
-            var degree = await _userService.GetUsersDegreeAsync(userId);
-            if (!await _degreeService.IsModulPartOfDegreeAsync(semesterPlannerModul.ModulId, degree.Id))
-            {
-                if (semesterPlannerModul.Modul == null)
-                {
-                    var modul = await _modulService.GetByIdAsync(semesterPlannerModul.ModulId);
-                    throw new ReferenceNotExistsException(nameof(Degree), nameof(Modul), modul?.Name ?? semesterPlannerModul.ModulId.ToString());
-                }
-                throw new ReferenceNotExistsException(nameof(Degree), nameof(Modul), semesterPlannerModul.Modul!.Name);
-            }
-
-            if (await ExistsAsync(semesterPlannerModul.ModulId, semesterPlannerModul.SemesterPlannerId))
-            {
-                throw new EntityAlreadyExistsException(nameof(SemesterPlannerModul), $"{nameof(SemesterPlannerModul.ModulId)} ({semesterPlannerModul.ModulId})", $"{nameof(SemesterPlannerModul.SemesterPlannerId)} ({semesterPlannerModul.SemesterPlannerId})");
-            }
-
-            if (await AlreadyCompletedModul(userId, semesterPlannerModul.ModulId, semesterPlannerModul.SemesterPlannerId))
-            {
-                throw new EntityAlreadyCompletedException(nameof(SemesterPlannerModul), degree.Name);
-            }
+            await ValidateUserIdAndSemesterId(userId, semesterPlannerModul.SemesterPlannerId);
+            await ValidateModulId(userId, semesterPlannerModul.ModulId);
+            await ValidateSemesterPlannerModulDoesNotExist(semesterPlannerModul.ModulId, semesterPlannerModul.SemesterPlannerId);
+            await ValidateModulNotAlreadyCompleted(userId, semesterPlannerModul.ModulId, semesterPlannerModul.SemesterPlannerId);
 
             _dbContext.SemesterPlannerModuls.Add(semesterPlannerModul);
             return await _dbContext.SaveChangesAsync();
@@ -74,30 +53,12 @@ namespace credit_wizard_api.Services
 
         public async Task<int> UpdateAsync(SemesterPlannerModul semesterPlannerModul, Guid userId)
         {
-            if (!await _semesterPlannerService.IsUsersPlannedSemester(userId, semesterPlannerModul.SemesterPlannerId))
-            {
-                throw new EntityNotFoundException($"User with Id {userId} has no matching planned semester with Id {semesterPlannerModul.SemesterPlannerId}");
-            }
-
-            var degree = await _userService.GetUsersDegreeAsync(userId);
-            if (!await _degreeService.IsModulPartOfDegreeAsync(semesterPlannerModul.ModulId, degree.Id))
-            {
-                if (semesterPlannerModul.Modul == null)
-                {
-                    var modul = await _modulService.GetByIdAsync(semesterPlannerModul.ModulId);
-                    throw new ReferenceNotExistsException(nameof(Degree), nameof(Modul), modul?.Name ?? semesterPlannerModul.ModulId.ToString());
-                }
-                throw new ReferenceNotExistsException(nameof(Degree), nameof(Modul), semesterPlannerModul.Modul!.Name);
-            }
-
-            if (await AlreadyCompletedModul(userId, semesterPlannerModul.ModulId, semesterPlannerModul.SemesterPlannerId))
-            {
-                throw new EntityAlreadyCompletedException(nameof(SemesterPlannerModul), degree!.Name);
-            }
+            await ValidateUserIdAndSemesterId(userId, semesterPlannerModul.SemesterPlannerId);
+            await ValidateModulId(userId, semesterPlannerModul.ModulId);
+            await ValidateModulNotAlreadyCompleted(userId, semesterPlannerModul.ModulId, semesterPlannerModul.SemesterPlannerId);
 
             var item = await GetByIdAsync(semesterPlannerModul.ModulId, semesterPlannerModul.SemesterPlannerId);
             if (item == null) throw new EntityNotFoundException($"No SemesterPlannerModul found with SemesterPlannerId {semesterPlannerModul.SemesterPlanner} & ModulId {semesterPlannerModul.ModulId}");
-
 
             item.Grade = semesterPlannerModul.Grade;
 
@@ -107,15 +68,12 @@ namespace credit_wizard_api.Services
 
         public async Task<int> DeleteAsync(Guid modulId, Guid semesterPlannerId, Guid userId)
         {
-            if (!await _semesterPlannerService.IsUsersPlannedSemester(userId, semesterPlannerId))
-            {
-                throw new EntityNotFoundException($"User with Id {userId} has no matching planned semester with Id {semesterPlannerId}");
-            }
+            await ValidateUserIdAndSemesterId(userId, semesterPlannerId);
 
-            var deleted = await GetByIdAsync(modulId, semesterPlannerId);
-            if (deleted == null) throw new EntityNotFoundException($"No SemesterPlannerModul found with SemesterPlannerId {semesterPlannerId} & ModulId {modulId}");
+            var toDelete = await GetByIdAsync(modulId, semesterPlannerId);
+            if (toDelete == null) throw new EntityNotFoundException($"No SemesterPlannerModul found with SemesterPlannerId {semesterPlannerId} & ModulId {modulId}");
 
-            _dbContext.SemesterPlannerModuls.Remove(deleted);
+            _dbContext.SemesterPlannerModuls.Remove(toDelete);
             return await _dbContext.SaveChangesAsync();
         }
 
@@ -140,6 +98,68 @@ namespace credit_wizard_api.Services
                     x.ModulId == modulId &&
                     x.SemesterPlannerId != semesterPlannerId &&
                     x.Grade >= 4);
+        }
+        
+        /// <summary>
+        /// Validates that the provided user ID corresponds to a planned semester with the provided semester planner ID.
+        /// Throws an EntityNotFoundException if no matching planned semester is found.
+        /// </summary>
+        /// <param name="userId">The ID of the user to validate.</param>
+        /// <param name="semesterPlannerId">The ID of the semester planner to </param>
+        /// <exception cref="EntityNotFoundException">when no entity is found</exception>
+        private async Task ValidateUserIdAndSemesterId(Guid userId, Guid semesterPlannerId)
+        {
+            if (!await _semesterPlannerService.IsUsersPlannedSemester(userId, semesterPlannerId))
+            {
+                throw new EntityNotFoundException($"User with Id {userId} has no matching planned semester with Id {semesterPlannerId}");
+            }
+        }
+        
+        /// <summary>
+        /// Validates if a given module ID exists as part of the user's degree.
+        /// </summary>
+        /// <param name="userId">The ID of the user to validate.</param>
+        /// <param name="modulId">The ID of the module to validate.</param>
+        /// <exception cref="ReferenceNotExistsException">Thrown when the module ID does not exist as part of the user's degree.</exception>
+        private async Task ValidateModulId(Guid userId, Guid modulId)
+        {
+            var degree = await _userService.GetUsersDegreeAsync(userId);
+            if (!await _degreeService.IsModulPartOfDegreeAsync(modulId, degree.Id))
+            {
+                var modul = await _modulService.GetByIdAsync(modulId);
+                throw new ReferenceNotExistsException(nameof(Degree), nameof(Modul), modul?.Name ?? modulId.ToString());
+            }
+        }
+        
+        /// <summary>
+        /// Validates whether a semester planner module with the specified module ID and semester planner ID already exists.
+        /// </summary>
+        /// <param name="modulId">The ID of the module to validate.</param>
+        /// <param name="semesterPlannerId">The ID of the semester planner to validate.</param>
+        /// <exception cref="EntityAlreadyExistsException">if a matching semester planner module already exists.</exception>
+        private async Task ValidateSemesterPlannerModulDoesNotExist(Guid modulId, Guid semesterPlannerId)
+        {
+            if (await ExistsAsync(modulId, semesterPlannerId))
+            {
+                throw new EntityAlreadyExistsException(nameof(SemesterPlannerModul), $"{nameof(SemesterPlannerModul.ModulId)} ({modulId})", $"{nameof(SemesterPlannerModul.SemesterPlannerId)} ({semesterPlannerId})");
+            }
+        }
+        
+        /// <summary>
+        /// Validates if the modul with the given ID is not already completed by the user in the given semester planner.
+        /// Throws an <see cref="EntityAlreadyCompletedException"/> if the modul is already completed.
+        /// </summary>
+        /// <param name="userId">The ID of the user to check for completion of the modul.</param>
+        /// <param name="modulId">The ID of the modul to check for completion.</param>
+        /// <param name="semesterPlannerId">The ID of the semester planner in which the modul is to be checked for completion.</param>
+        /// <exception cref="EntityAlreadyCompletedException">Thrown when the modul is already completed by the user.</exception>
+        private async Task ValidateModulNotAlreadyCompleted(Guid userId, Guid modulId, Guid semesterPlannerId)
+        {
+            if (await AlreadyCompletedModul(userId, modulId, semesterPlannerId))
+            {
+                var degree = await _userService.GetUsersDegreeAsync(userId);
+                throw new EntityAlreadyCompletedException(nameof(SemesterPlannerModul), degree.Name);
+            }
         }
     }
 }
